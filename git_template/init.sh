7a4ed7a7f2536c0011d1030f4ef69daf79d9be50
ahuigo <1781999+ahuigo@users.noreply.github.com>
2022-06-27 20:08:25 +0800

    feat: add git commit template

---
+++ git_template/init.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env zsh
+###################### parse argv ###################
+parse_argv(){
+    while test $# -gt 0; do
+        case "$1" in
+            (-f) FORCE=1;;
+            (-s) SOFT_LINK=1;;
+            (*) 
+                cat <<-'MM'
+USAGE:
+    ./init.sh [OPTIONS]
+OPTIONS:
+    -f Force override old ~/git_template(auto backup)
+    -s Use symbolic link
+MM
+                exit 1;;
+        esac
+        shift
+    done
+    return 1
+}
+parse_argv "$@"
+
+####################### shell type ####################
+getShellType(){
+    if [[ -n $BASH_VERSION ]] || [[ -n $BASH_SOURCE ]]; then
+        echo bash
+    elif [[ -n $ZSH_VERSION ]]; then
+        echo zsh
+    fi
+}
+SHELL_TYPE=`getShellType`
+isZsh(){
+    if [[ $SHELL_TYPE = 'zsh' ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+isBash(){
+    if [[ $SHELL_TYPE = 'bash' ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+####################### script  path ####################
+if isZsh; then
+    CWD=$(dirname ${0:a})
+elif isBash; then
+    CWD=$(dirname ${BASH_SOURCE[0]})
+    CWD=$(cd $CWD;pwd)
+else
+    echo "This script only support zsh and bash!!"
+    return 1
+fi
+if ! ls $CWD/init.sh >/dev/null;then
+    exit 1
+fi
+
+####################### link script####################
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+if test "$FORCE" = '1' && test -d $HOME/.git_template ;then
+    echo mv ~/.git_template ~/.git_template.bak
+    mv ~/.git_template ~/.git_template.bak
+fi
+if ! { [[ -L $HOME/.git_template ]] || [[ -d $HOME/.git_template ]];};then
+    if test $SOFT_LINK = 1; then
+        if ! [[ -L $HOME/.git_template ]];then
+            ln -s $CWD ~/.git_template
+        fi
+    else
+        if ! [[ -d $HOME/.git_template ]];then
+            #ln -s $CWD ~/.git_template
+            mkdir ~/.git_template
+            cp -r $CWD/ ~/.git_template
+        fi
+    fi
+    git config --global init.templatedir '~/.git_template'
+    git config --global commit.template '~/.git_template/gitmessage'
+
+    printf "Installed ${RED}~/.git_template/${NC} successfully.\n"
+else
+    printf "You've installed ${RED}~/.git_template${NC} before.\n"
+    printf "You could run **init.sh -f** to override old template(auto backup)\n"
+fi
--- git_template/readme.md
+++ git_template/readme.md
@@ -1,11 +1,49 @@
-# git tools
+# git template
+What this template contains?
+1. pre-commit hook:  
+it supports automatic test(go/python/java/nodejs/deno/...) before `git commit` execution.
+2. git commit message template
 
-## global template
-This templatedir provides common git hooks like pre-commit 
+## 1-Click install
+1-Click install all git tools:
 
-Initial templatedir:
+    # install global template
+    bash ./git_template/init.sh
 
-    git config --global init.templatedir '~/.git_template'
-    ln -s `pwd` ~/.git_template
-    # mkdir ~/.git_template
-    # cp -r . ~/.git_template
+    # import template to old project
+    cp -r ~/.git_template/ .git/
+
+Force install:
+
+    # With `-f` option, it will override old `git_template` and backup it automatically
+    bash ./git_template/init.sh -f
+
+For help:
+
+    ./git_template/init.sh -h
+
+## Usage
+### Auto code test
+This template includes hook for performing automated tests.
+
+Just run:
+
+    $ cd golang_project
+    $ git commit
+    ....
+    go test ./....
+
+## Commit template
+Just run:
+
+    $ git commit 
+    # Commit messages rule:
+    # 
+    #     <type>(<scope>): <subject>
+    #     <BLANK LINE>
+    #     <body>
+    #     <BLANK LINE>
+    #     <footer>
+    #
+    # Example 1:
+    # ....
